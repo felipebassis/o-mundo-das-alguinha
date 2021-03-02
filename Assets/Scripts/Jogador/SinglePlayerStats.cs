e19d420831f195cd4b6923307b5411f244b7726b
Si3gh <jp.b.pissolito@gmail.com>
2021-03-01 23:22:39 -0300

    Implementação de status do jogador && Atribuição de novas sprites/imagens

--- Assets/Scripts/Jogador/SinglePlayerStats.cs
+++ Assets/Scripts/Jogador/SinglePlayerStats.cs
@@ -1,21 +1,48 @@
-﻿using UnityEngine;
+﻿using System;
+using TMPro;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Jogador
 {
 	public class SinglePlayerStats : MonoBehaviour
 	{
-		[SerializeField] private Player[] players;
-		[SerializeField] private Text playersDisplayedName;
+		private Player player;
+		[SerializeField] private TMP_Text playerPosition;
+		[SerializeField] private TMP_Text playersDisplayedName;
 		[SerializeField] private Image playersSpriteImage;
-		[SerializeField] private Color playerColor;
-	
-		private int _currentPlayerIndex;
+		[SerializeField] private Image playerColor;
 
-		public void GetPlayerStats()
+		[SerializeField] private GameObject display;
+		private bool active;
+
+		public void UpdatePlayerPosition()
 		{
-
+			playerPosition.text = player.GetPosition().ToString();
 		}
 		
+		//TODO "Setar" esse loco no turno.
+		public void SetPlayer(Player player)
+		{
+			active = true;
+			display.SetActive(true);
+			this.player = player;
+			playerColor.color = player.GetCharacter().GetColor();
+			playersDisplayedName.text = player.GetCharacter().GetName();
+		}
+
+		private void Update()
+		{
+			if (active)
+			{
+				UpdatePlayerPosition();
+			}
+		}
+
+		private void Awake()
+		{
+			display.SetActive(false);
+			active = false;
+		}
 	}
 }
